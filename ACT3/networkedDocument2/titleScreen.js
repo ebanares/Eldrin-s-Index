e2699f40885fc1abbbce5ee90816fa027bd68bb7
Eldrin Banares <55264397+ebanares@users.noreply.github.com>
2019-12-02 20:33:10 -0500

    Update titleScreen.js

--- ACT3/networkedDocument2/titleScreen.js
+++ ACT3/networkedDocument2/titleScreen.js
@@ -1,57 +1,82 @@
 console.log("im alive");
 
-
 function dogIn(){
-		$("body").css("background-color","#ffcccc");
-		$(".catBG, .birdBG, .fishBG").css("color","#ffcccc");
-		$(".dogBG, body").css("color","black");
-		$(".rule").css("border","2px solid black")
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-01.jpg')");
+		$(".catBG, .birdBG, .fishBG").css("opacity","0");
+		$(".dogBG, body").css("color","#b30000");
+		$(".rule").css("border","2px solid #b30000")
 		$("body").css("transition-duration","0.5s");
+		
 	}
 function dogOut(){
-		$("body").css("background-color","#777");
-		$(".dogBG, .catBG, .birdBG, .fishBG").css("color","white");
-		$(".dogBG, body").css("color","white");
-		$(".rule").css("border","2px solid white")
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-05.jpg')");
+		$(".dogBG, .catBG, .birdBG, .fishBG").css("opacity","1");
+		$(".dogBG, body").css("color","#b35c00");
+		$(".rule").css("border","2px solid #b35c00");
+
 	}
 function catIn(){
-		$("body").css("background-color","#ddccff");
-		$(".dogBG, .birdBG, .fishBG").css("color","#ddccff");
-		$(".catBG, body").css("color","black");
-		$(".rule").css("border","2px solid black")
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-02.jpg')");
+		$(".dogBG, .birdBG, .fishBG").css("opacity","0");
+		$(".catBG, body").css("color","#5900b3");
+		$(".rule").css("border","2px solid #5900b3");
 		$("body").css("transition-duration","0.5s");
 	}
 function catOut(){
-		$("body").css("background-color","#777");
-		$(".dogBG, .birdBG, .fishBG").css("color","white");
-		$(".catBG, body").css("color","white");
-		$(".rule").css("border","2px solid white");
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-05.jpg')");
+		$(".dogBG, .birdBG, .fishBG").css("opacity","1");
+		$(".catBG, body").css("color","#b35c00");
+		$(".rule").css("border","2px solid #b35c00");
 	}
 function birdIn(){
-		$("body").css("background-color","#ccffcc");
-		$(".dogBG, .catBG, .fishBG").css("color","#ccffcc");
-		$(".birdBG, body").css("color","black");
-		$(".rule").css("border","2px solid black")
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-03.jpg')");
+		$(".dogBG, .catBG, .fishBG").css("opacity","0");
+		$(".birdBG, body").css("color","#00b300");
+		$(".rule").css("border","2px solid #00b300");
 		$("body").css("transition-duration","0.5s");
 	}
 function birdOut(){
-		$("body").css("background-color","#777");
-		$(".dogBG, .catBG, .fishBG").css("color","white");
-		$(".birdBG, body").css("color","white");
-		$(".rule").css("border","2px solid white");
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-05.jpg')");
+		$(".dogBG, .catBG, .fishBG").css("opacity","1");
+		$(".birdBG, body").css("color","#b35c00");
+		$(".rule").css("border","2px solid #b35c00");
 	}
 function fishIn(){
-		$("body").css("background-color","#cce6ff");
-		$(".dogBG, .catBG, .birdBG").css("color","#cce6ff");
-		$(".fishBG, body").css("color","black");
-		$(".rule").css("border","2px solid black")
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-04.jpg')");
+		$(".dogBG, .catBG, .birdBG").css("opacity","0");
+		$(".fishBG, body").css("color","#0077b3");
+		$(".rule").css("border","2px solid #0077b3");
 		$("body").css("transition-duration","0.5s");
 	}
 function fishOut(){
-		$("body").css("background-color","#777");
-		$(".dogBG, .catBG, .birdBG").css("color","white");
-		$(".fishBG, body").css("color","white");
-		$(".rule").css("border","2px solid white");
+		$("body").css("background-repeat", "no-repeat")
+				.css("background-size", "cover")
+				.css("background-position", "center")
+				.css("background-image", "url('final_resources/bg-05.jpg')");
+		$(".dogBG, .catBG, .birdBG").css("opacity","1");
+		$(".fishBG, body").css("color","#b35c00");
+		$(".rule").css("border","2px solid #b35c00");
 	}
 window.onload = function(){
 	$(".dogBG").hover(dogIn, dogOut);
